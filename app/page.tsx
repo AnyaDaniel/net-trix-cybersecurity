dee3eec53065a3000c6f8c362e3c327c15b54a40
Anne8080 <ireakin10@gmail.com>
2024-08-08 23:43:51 +0100

    starting

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      <h1>NET TIRX CYBERSECURITY</h1>
+      <h1>NET TRIX CYBERSECURITY</h1>
     </main>
   )
 }
